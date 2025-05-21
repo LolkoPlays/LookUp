84c3e2d3d1ccc453a3ba7d3937665d520761b4b7
NotLolko <103894710+LolkoPlays@users.noreply.github.com>
2025-05-21 11:54:50 +0200

    Create entries.js

---
+++ examplePack/scripts/imports/entries.js
@@ -0,0 +1,459 @@
+export const entries = {
+    //Night Vision
+    "potions:golden_carrot": {
+        entryData: {
+            hidden: true,
+            name: "Golden Carrot",
+            category: "Potions",
+            icon: "textures/items/carrot_golden"
+        }
+    },
+    "potions:night_vision_potion": {
+        entryData: {
+            name: "Potion of Night Vision",
+            desc: ["§7Night Vision (3:00)"],
+            label: "Night\nVision",
+            page: "Potions",
+            category: "Potions",
+            icon: "textures/items/potion_bottle_nightVision"
+        }
+    },
+    //Invisibility
+    "potions:invisibility_potion": {
+        entryData: {
+            name: "Potion of Invisibility",
+            desc: ["§7Invisibility (3:00)"],
+            label: "Invisibility",
+            page: "Potions",
+            category: "Potions",
+            icon: "textures/items/potion_bottle_invisibility"
+        }
+    },
+    //Leaping
+    "potions:rabbit_foot": {
+        entryData: {
+            hidden: true,
+            name: "Rabbit Foot",
+            category: "Potions",
+            icon: "textures/items/rabbit_foot" 
+        }
+    },
+    "potions:leaping_potion": {
+        entryData: {
+            name: "Potion of Leaping",
+            desc: ["§7Jump Boost (3:00)"],
+            label: "Leaping",
+            page: "Potions",
+            category: "Potions",
+            icon: "textures/items/potion_bottle_jump"
+        }
+    },
+    //Fire Resistance
+    "potions:magma_cream": {
+        entryData: {
+            hidden: true,
+            name: "Magma Cream",
+            category: "Potions",
+            icon: "textures/items/magma_cream" 
+        }
+    },
+    "potions:fire_resistance_potion": {
+        entryData: {
+            name: "Potion of Fire Resistance",
+            desc: ["§7Fire Resistance (3:00)"],
+            label: "Fire\nResistance",
+            page: "Potions",
+            category: "Potions",
+            icon: "textures/items/potion_bottle_fireResistance"
+        }
+    },
+    //Swiftness
+    "potions:sugar": {
+        entryData: {
+            hidden: true,
+            name: "Sugar",
+            category: "Potions",
+            icon: "textures/items/sugar"
+        } 
+    },
+    "potions:swiftness_potion": {
+        entryData: {
+            name: "Potion of Swiftness",
+            desc: ["§7Speed (3:00)\n\n§5When Applied:\n§9+20%% Speed"],
+            label: "Swiftness",
+            page: "Potions",
+            category: "Potions",
+            icon: "textures/items/potion_bottle_moveSpeed"
+        }
+    },
+    //Slowness
+    "potions:slowness_potion": {
+        entryData: {
+            name: "Potion of Slowness",
+            desc: ["§cSlowness (1:30)\n\n§5When Applied:\n§c-15%% Speed"],
+            label: "Slowness",
+            page: "Potions",
+            category: "Potions",
+            icon: "textures/items/potion_bottle_moveSlowdown"
+        }
+    },
+    //Water Breathing
+    "potions:water_breathing_potion": {
+        entryData: {
+            name: "Potion of Water Breathing",
+            desc: ["§7Water Breathing (3:00)"],
+            label: "Water\nBreathing",
+            page: "Potions",
+            category: "Potions",
+            icon: "textures/items/potion_bottle_waterBreathing"
+        }
+    },
+    //Healing
+    "potions:glistering_melon_slice": {
+        entryData: {
+            hidden: true,
+            name: "Glistering Melon Slice",
+            category: "Potions",
+            icon: "textures/items/melon_speckled" 
+        }
+    },
+    "potions:healing_potion": {
+        entryData: {
+            name: "Potion of Healing",
+            desc: ["§7Instant Health"],
+            label: "Healing",
+            page: "Potions",
+            category: "Potions",
+            icon: "textures/items/potion_bottle_heal"
+        }
+    },
+    //Harming
+    "potions:harming_potion": {
+        entryData: {
+            name: "Potion of Harming",
+            desc: ["§cInstant Damage"],
+            label: "Harming",
+            page: "Potions",
+            category: "Potions",
+            icon: "textures/items/potion_bottle_harm"
+        }
+    },
+    //Poison
+    "potions:spider_eye": {
+        entryData: {
+            hidden: true,
+            name: "Spider Eye",
+            category: "Potions",
+            icon: "textures/items/spider_eye"
+        }
+    },
+    "potions:poison_potion": {
+        entryData: {
+            name: "Potion of Poison",
+            desc: ["§cPoison (0:45)"],
+            label: "Poison",
+            page: "Potions",
+            category: "Potions",
+            icon: "textures/items/potion_bottle_poison"
+        }
+    },
+    //Regeneration
+    "potions:ghast_tear": {
+        entryData: {
+            hidden: true,
+            name: "Ghast Tear",
+            category: "Potions",
+            icon: "textures/items/ghast_tear"
+        } 
+    },
+    "potions:regeneration_potion": {
+        entryData: {
+            name: "Potion of Regeneration",
+            desc: ["§7Regeneration (0:45)"],
+            label: "Health\nRegen",
+            page: "Potions",
+            category: "Potions",
+            icon: "textures/items/potion_bottle_regeneration"
+        }
+    },
+    //Strength
+    "potions:blaze_rod": {
+        entryData: {
+            hidden: true,
+            name: "Blaze Rod",
+            category: "Potions",
+            icon: "textures/items/blaze_rod"
+        } 
+    },
+    "potions:blaze_powder": {
+        entryData: {
+            hidden: true,
+            name: "Blaze Powder",
+            category: "Potions",
+            icon: "textures/items/blaze_powder"
+        }
+    },
+    "potions:strength_potion": {
+        entryData: {
+            name: "Potion of Strength",
+            desc: ["§7Strength (3:00)\n\n§5When Applied:\n§9+1.3 Attack Damage"],
+            label: "Strength",
+            page: "Potions",
+            category: "Potions",
+            icon: "textures/items/potion_bottle_damageBoost"
+        }
+    },
+    //Weakness
+    "potions:weakness_potion": {
+        entryData: {
+            name: "Potion of Weakness",
+            desc: ["§cWeakness (1:30)\n\n§5When Applied:\n§c-0.7 Attack Damage"],
+            label: "Weakness",
+            page: "Potions",
+            category: "Potions",
+            icon: "textures/items/potion_bottle_weakness"
+        }
+    },
+    //Turtle Master
+    "potions:turtle_helmet": {
+        entryData: {
+            hidden: true,
+            name: "Turtle Shell",
+            category: "Potions",
+            icon: "textures/items/turtle_helmet"
+            
+        }
+    },
+    "potions:turtle_master_potion": {
+        entryData: {
+            name: "Potion of the Turtle Master",
+            desc: ["§cSlowness IV (0:20)\n§7Resistance III (0:20)\n\n§5When Applied:\n§c-60%% Speed"],
+            label: "Turtle\nMaster",
+            page: "Potions",
+            category: "Potions",
+            icon: "textures/items/potion_bottle_turtleMaster"
+        }
+    },
+    //Slow Falling
+    "potions:phantom_membrane": {
+        entryData: {
+            hidden: true,
+            name: "Phantom Membrane",
+            category: "Potions",
+            icon: "textures/items/phantom_membrane"
+        }
+    },
+    "potions:slow_falling_potion": {
+        entryData: {
+            name: "Potion of Slow Falling",
+            desc: ["§7Slow Falling (1:30)"],
+            label: "Slow\nFalling",
+            page: "Potions",
+            category: "Potions",
+            icon: "textures/items/potion_bottle_slowFall"
+        }
+    },
+    //Wind Charging
+    "potions:breeze_rod": {
+        entryData: {
+            hidden: true,
+            name: "Breeze Rod",
+            category: "Potions",
+            icon: "textures/items/breeze_rod"
+        }
+    },
+    "potions:wind_charging_potion": {
+        entryData: {
+            name: "Potion of Wind Charging",
+            desc: ["§cWind Charged (3:00)"],
+            label: "Wind\nCharging",
+            page: "Potions",
+            category: "Potions",
+            icon: "textures/items/potion_bottle_windCharged"
+        }
+    },
+    //Weaving
+    "potions:weaving_potion": {
+        entryData: {
+            name: "Potion of Weaving",
+            desc: ["§cWeaving (3:00)"],
+            label: "Weaving",
+            page: "Potions",
+            category: "Potions",
+            icon: "textures/items/potion_bottle_weaving"
+        }
+    },
+    //Oozing
+    "potions:slime": {
+        entryData: {
+            hidden: true,
+            name: "Slime Block",
+            category: "Potions",
+            icon: "minecraft:slime"
+        }
+    },
+    "potions:oozing_potion": {
+        entryData: {
+            name: "Potion of Oozing",
+            desc: ["§cOozing (3:00)"],
+            label: "Oozing",
+            page: "Potions",
+            category: "Potions",
+            icon: "textures/items/potion_bottle_oozing"
+        }
+    },
+    //Infestation
+    "potions:stone": {
+        entryData: {
+            hidden: true,
+            name: "Stone",
+            category: "Potions",
+            icon: "minecraft:stone"
+        }
+    },
+    "potions:infestation_potion": {
+        entryData: {
+            name: "Potion of Infestation",
+            desc: ["§cInfested (3:00)"],
+            label: "Infestation",
+            page: "Potions",
+            category: "Potions",
+            icon: "textures/items/potion_bottle_infested"
+        }
+    },
+    //Awkard Potion
+    "potions:awkard_potion": {
+        entryData: {
+            name: "Awkard Potion",
+            desc: "\nA basic potion brewing component",
+            label: "Awkard\nPotion",
+            page: "Ingredients",
+            category: "Potions",
+            icon: "textures/items/potion_bottle_drinkable"
+        }
+    },
+    //Mundane Potion
+    "potions:raw_gold": {
+        entryData: {
+            hidden: true,
+            name: "Raw Gold",
+            category: "Potions",
+            icon: "textures/items/raw_gold"
+        }
+    },
+    "potions:gold_ingot": {
+        entryData: {
+            hidden: true,
+            name: "Gold Ingot",
+            category: "Potions",
+            icon: "textures/items/gold_ingot"
+        }
+    },
+    "potions:gold_nugget": {
+        entryData: {
+            hidden: true,
+            name: "Gold Nugget",
+            category: "Potions",
+            icon: "textures/items/gold_nugget"
+        }
+    },
+    "potions:slimeball": {
+        entryData: {
+            hidden: true,
+            name: "Slimeball",
+            category: "Potions",
+            icon: "textures/items/slimeball"
+        }
+    },
+    "potions:mundane_potion": {
+        entryData: {
+            name: "Mundane Potion",
+            desc: "\nA basic potion brewing component",
+            label: "Mundane\nPotion",
+            page: "Ingredients",
+            category: "Potions",
+            icon: "textures/items/potion_bottle_drinkable"
+        }
+    },
+    //Thick Potion
+    "potions:thick_potion": {
+        entryData: {
+            name: "Thick Potion",
+            desc: "\nA basic potion brewing component",
+            label: "Thick\nPotion",
+            page: "Ingredients",
+            category: "Potions",
+            icon: "textures/items/potion_bottle_drinkable"
+        }
+    },
+    //Water Bottle
+    "potions:glass": {
+        entryData: {
+            hidden: true,
+            name: "Glass",
+            category: "Potions",
+            icon: "minecraft:glass"
+        }
+    },
+    "potions:glass_bottle": {
+        entryData: {
+            hidden: true,
+            name: "Glass Bottle",
+            category: "Potions",
+            icon: "textures/items/potion_bottle_empty"
+        }
+    },
+    "potions:water_bottle": {
+        entryData: {
+            hidden: true,
+            name: "Water Bottle",
+            category: "Potions",
+            icon: "textures/items/potion_bottle_drinkable"
+        }
+    },
+    //Enhance
+    "potions:glowstone_dust": {
+        entryData: {
+            hidden: true,
+            name: "Glowstone Dust",
+            category: "Potions",
+            icon: "textures/items/glowstone_dust"
+        }
+    },
+    //Extend
+    "potions:redstone": {
+        entryData: {
+            hidden: true,
+            name: "Redstone Dust",
+            category: "Potions",
+            icon: "textures/items/redstone_dust"
+        } 
+    },
+    //Splash
+    "potions:gunpowder": {
+        entryData: {
+            hidden: true,
+            name: "Gunpowder",
+            category: "Potions",
+            icon: "textures/items/gunpowder"
+        }
+    },
+    //Lingering
+    "potions:dragon_breath": {
+        entryData: {
+            hidden: true,
+            name: "§eDragon's Breath",
+            category: "Potions",
+            icon: "textures/items/dragons_breath"
+        }
+    },
+    //Corrupt
+    "potions:fermented_spider_eye": {
+        entryData: {
+            hidden: true,
+            name: "Fermented Spider Eye",
+            category: "Potions",
+            icon: "textures/items/spider_eye_fermented"
+        }
+    }
+}

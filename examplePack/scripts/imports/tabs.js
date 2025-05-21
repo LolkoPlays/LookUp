cb6560670b5024215c0382b04bc9bc62e67ffca3
NotLolko <103894710+LolkoPlays@users.noreply.github.com>
2025-05-21 11:54:18 +0200

    Create tabs.js

---
+++ examplePack/scripts/imports/tabs.js
@@ -0,0 +1,11 @@
+export const tabs = {
+    "Potions": {
+        tabData: {
+            tab: "Main",
+            menu: "Potions",
+            icon: "textures/items/blaze_rod",
+            body: "Select potion:",
+            header: "§lPotion Info"
+        }
+    }
+}
